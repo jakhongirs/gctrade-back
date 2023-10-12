9e1859f430b7fd5f866f01fa9ca6aa7f9d2af874
jakhongirs <jakhongirsv@gmail.com>
2023-10-12 23:40:39 +0500

    fix: swagger info

--- core/schema.py
+++ core/schema.py
@@ -7,11 +7,11 @@ from .generator import BothHttpAndHttpsSchemaGenerator
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="UIC API",
+        title="GCTRADE API",
         default_version="v1",
-        description="UIC Group",
+        description="GCTRADE API",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="info@uic.group"),
+        contact=openapi.Contact(email="jakhongirsv@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
