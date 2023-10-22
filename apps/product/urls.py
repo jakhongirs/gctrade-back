0e7a802378093d855ad2163a1c76054681bbff5f
jakhongirs <jakhongirsv@gmail.com>
2023-10-22 21:33:49 +0500

    feat: hot fix

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -7,11 +7,11 @@ from .views import (BannerListView, LastSeenProductListView,
 app_name = "product"
 
 urlpatterns = [
+    path("list/", ProductListView.as_view(), name="products-list"),
+    path("detail/<slug:slug>/", ProductDetailView.as_view(), name="products-detail"),
     path("banner/", BannerListView.as_view(), name="banner-list"),
     path("manufacturer/", ManufacturerListView.as_view(), name="manufacturer-list"),
     path("categories/", ParentCategoryListView.as_view(), name="categories-list"),
-    path("products/", ProductListView.as_view(), name="products-list"),
-    path("products/<slug:slug>/", ProductDetailView.as_view(), name="products-detail"),
     path("last-seen-products/", LastSeenProductListView.as_view(), name="last-seen-products-list"),
     path("manufacturer/<int:category_id>/", ManufacturerByCategoryListView.as_view(), name="manufacturer-by-category"),
 ]
