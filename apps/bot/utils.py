ecf87c3f996f3a5f78a2af6a54c576aef09f5da1
Jakhongir Sagdullaev <jakhongirs@Jakhongirs-iMac.local>
2023-10-25 15:25:27 +0500

    feat: generate pdf from html

--- apps/bot/utils.py
+++ apps/bot/utils.py
@@ -1,11 +1,10 @@
+import io
 import os
+from pathlib import Path
 
 import requests
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from django.template.loader import get_template
+from weasyprint import HTML
 
 from apps.product.models import Order
 
@@ -18,7 +17,9 @@ def bot_send_message(message, order_id=None):
         order = Order.objects.get(pk=order_id)
 
     # Create PDF file
-    pdf_file_path = create_pdf(order)
+    pdf_file_path = create_order_pdf(order)
+
+    print(pdf_file_path)
 
     # Send PDF file as a document to Telegram
     files = {"document": open(pdf_file_path, "rb")}
@@ -30,56 +31,21 @@ def bot_send_message(message, order_id=None):
     os.remove(pdf_file_path)
 
 
-def create_pdf(order_instance):
-    pdf_file_path = f"order_{order_instance.pk}.pdf"
-    pdfmetrics.registerFont(TTFont("NotoSans-Regular", "staticfiles/assets/fonts/NotoSans-Regular.ttf"))
+def create_order_pdf(order):
+    template = get_template("order.html")
 
-    # Create a canvas object
-    c = SimpleDocTemplate(pdf_file_path, pagesize=letter)
+    context = {
+        "order": order,
+    }
 
-    # Order details
-    order_details = [
-        ["🏷️ Status", order_instance.get_status_display()],
-        ["🆔 Buyurtma ID", order_instance.pk],
-        ["👤 Ism", order_instance.name],
-        ["📞 Telefon", order_instance.phone],
-        ["📅 Sana", order_instance.created_at.strftime("%Y-%m-%d %H:%M")],
-        ["🧾 Jami", order_instance.cart.total_price],
-        ["", ""],
-        ["🛒 Savatcha:", ""],
-        ["📦 Mahsulot", "💰 Narxi", "📋 Soni", "🧾 Jami"],  # Header for cart items
-    ]
+    html = template.render(context)
+    output = io.BytesIO()
+    HTML(string=html, base_url=os.getcwd()).write_pdf(output)
 
-    # Cart items
-    cart_items = order_instance.cart.items.all()
-    for product in cart_items:
-        item_row = [
-            product.product.title,
-            product.product.price,
-            product.quantity,
-            product.quantity * product.product.price,
-        ]
-        order_details.append(item_row)
+    # save file inside root directory
+    file_path = Path(__file__).resolve().parent.parent.parent / f"order_{order.pk}.pdf"
 
-    # Create a table style
-    style = TableStyle(
-        [
-            ("BACKGROUND", (0, 0), (-1, 0), colors.HexColor("#77aaff")),
-            ("TEXTCOLOR", (0, 0), (-1, 0), colors.HexColor("#ffffff")),
-            ("ALIGN", (0, 0), (-1, -1), "LEFT"),
-            ("FONTNAME", (0, 0), (-1, -1), "NotoSans-Regular"),
-            ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-            ("BACKGROUND", (0, 1), (-1, -1), colors.HexColor("#f7f7f7")),
-        ]
-    )
+    with open(file_path, "wb") as pdf_file:
+        pdf_file.write(output.getvalue())
 
-    # Create the table and apply the style
-    table = Table(order_details)
-    print(table)
-    table.setStyle(style)
-
-    # Build the PDF
-    elements = [table]
-    c.build(elements)
-
-    return pdf_file_path
+    return file_path
