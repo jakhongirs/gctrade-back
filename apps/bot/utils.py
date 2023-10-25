a5ec6a289424a637c86dbe8d23c112c842aa4cbb
Jakhongir Sagdullaev <jakhongirs@Jakhongirs-iMac.local>
2023-10-25 12:19:32 +0500

    generate pdf

--- apps/bot/utils.py
+++ apps/bot/utils.py
@@ -1,8 +1,11 @@
 import os
 
 import requests
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 from apps.product.models import Order
 
@@ -29,15 +32,54 @@ def bot_send_message(message, order_id=None):
 
 def create_pdf(order_instance):
     pdf_file_path = f"order_{order_instance.pk}.pdf"
-    c = canvas.Canvas(pdf_file_path, pagesize=letter)
+    pdfmetrics.registerFont(TTFont("NotoSans-Regular", "staticfiles/assets/fonts/NotoSans-Regular.ttf"))
 
-    # Add order details to PDF
-    c.drawString(100, 750, f"🏷️ Status: {order_instance.get_status_display()}")
-    c.drawString(100, 730, f"🆔 Buyurtma ID: {order_instance.pk}")
-    c.drawString(100, 710, f"👤 Ism: {order_instance.name}")
-    c.drawString(100, 690, f"📞 Telefon: {order_instance.phone}")
-    c.drawString(100, 670, f"📅 Sana: {order_instance.created_at.strftime('%Y-%m-%d %H:%M')}")
-    c.drawString(100, 650, f"🧾 Jami: {order_instance.cart.total_price}")
+    # Create a canvas object
+    c = SimpleDocTemplate(pdf_file_path, pagesize=letter)
 
-    c.save()
+    # Order details
+    order_details = [
+        ["🏷️ Status", order_instance.get_status_display()],
+        ["🆔 Buyurtma ID", order_instance.pk],
+        ["👤 Ism", order_instance.name],
+        ["📞 Telefon", order_instance.phone],
+        ["📅 Sana", order_instance.created_at.strftime("%Y-%m-%d %H:%M")],
+        ["🧾 Jami", order_instance.cart.total_price],
+        ["", ""],
+        ["🛒 Savatcha:", ""],
+        ["📦 Mahsulot", "💰 Narxi", "📋 Soni", "🧾 Jami"],  # Header for cart items
+    ]
+
+    # Cart items
+    cart_items = order_instance.cart.items.all()
+    for product in cart_items:
+        item_row = [
+            product.product.title,
+            product.product.price,
+            product.quantity,
+            product.quantity * product.product.price,
+        ]
+        order_details.append(item_row)
+
+    # Create a table style
+    style = TableStyle(
+        [
+            ("BACKGROUND", (0, 0), (-1, 0), colors.HexColor("#77aaff")),
+            ("TEXTCOLOR", (0, 0), (-1, 0), colors.HexColor("#ffffff")),
+            ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+            ("FONTNAME", (0, 0), (-1, -1), "NotoSans-Regular"),
+            ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+            ("BACKGROUND", (0, 1), (-1, -1), colors.HexColor("#f7f7f7")),
+        ]
+    )
+
+    # Create the table and apply the style
+    table = Table(order_details)
+    print(table)
+    table.setStyle(style)
+
+    # Build the PDF
+    elements = [table]
+    c.build(elements)
+
     return pdf_file_path
