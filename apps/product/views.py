e8a40f69d31cb3d23adb2515cbddfd2d19dcf8a4
jakhongirs <jakhongirsv@gmail.com>
2023-11-05 15:48:52 +0500

    feat: filter by slug in  ParentCategoryListView

--- apps/product/views.py
+++ apps/product/views.py
@@ -38,7 +38,7 @@ class ParentCategoryListView(generics.ListAPIView):
     queryset = ParentCategory.objects.all()
     serializer_class = ParentCategorySerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ("id", "categories__id")
+    filterset_fields = ("id", "categories__id", "slug", "categories__slug")
 
 
 class ProductListView(generics.ListAPIView):
