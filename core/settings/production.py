4a1fd9acca2cbf1a17a55a7c312118d6e9020b43
Jakhongir Sagdullaev <60522700+jakhongirs@users.noreply.github.com>
2023-11-28 14:12:24 +0500

    Update production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -14,7 +14,7 @@ USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["https://gctrade.uz"]
+CSRF_TRUSTED_ORIGINS = ["https://gctrade.uz/"]
 
 ###################################################################
 # CORS
